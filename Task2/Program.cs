9060f390b91a97f388d7ae68e0ce1cb66e936e01
mila <milavoronina1997@mail.ru>
2022-09-22 19:09:03 +0500

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int SumOfNumbers(int M, int N)
+{
+    if(M > N) return 0;
+    return M + SumOfNumbers( M + 1, N);   
+}
+try
+{
+
+    Console.WriteLine("Введите первое целое натуральное число:");
+    int M = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе целое натуральное число:");
+    int N = Convert.ToInt32(Console.ReadLine());
+    if(M > 0 && N > 0)
+    {
+        Console.WriteLine(SumOfNumbers(M,N));
+    }
+    else
+    {
+        Console.WriteLine("Введите, пожалуйста, положительное число, так как натуральные числа являются только положительными числами");
+    }
+}
+catch
+{
+    Console.WriteLine("Введите, пожалуйста, целое натуральное число.");
+}
+
